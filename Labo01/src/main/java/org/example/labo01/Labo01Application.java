51f6e6300894b05c583565998eb4bf4deee9ca11
CarlosRubio637 <00000822@uca.edu.sv>
2026-04-20 17:10:55 -0600

    Feat: Se agrego el PokémonRunner

--- Labo01/src/main/java/org/example/labo01/Labo01Application.java
+++ Labo01/src/main/java/org/example/labo01/Labo01Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Labo01Application {
+public class  Labo01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Labo01Application.class, args);
--- Labo01/src/main/java/org/example/labo01/component/PokémonRunner.java
+++ Labo01/src/main/java/org/example/labo01/component/PokémonRunner.java
@@ -1,4 +1,42 @@
 package org.example.labo01.component;
 
-public class PokémonRunner {
+import lombok.RequiredArgsConstructor;
+import org.example.labo01.domain.entities.Pokémon;
+import org.example.labo01.service.PokémonService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class PokémonRunner implements CommandLineRunner {
+
+
+    private final PokémonService pokémonService;
+
+    @Override
+    public void run(String... args) {
+        System.out.println("====== INFORME TÉCNICO DEL PROFESOR OAK ======");
+
+        System.out.println("\n>>> FILTRANDO POR TIPO: Electrico");
+        imprimirLista(pokémonService.filtrarPorTipo("Electrico"));
+
+        System.out.println("\n>>> FILTRANDO POR ZONA: Hierva");
+        imprimirLista(pokémonService.filtrarPorZona("hierva"));
+
+        System.out.println("\n>>> FILTRANDO POR DEBILIDAD: Agua");
+        imprimirLista(pokémonService.filtrarPorDebilidad("Agua"));
+    }
+
+    private void imprimirLista(List<Pokémon> lista) {
+        if (lista.isEmpty()) {
+            System.out.println("[!] No se encontraron Pokémon con esos criterios.");
+        } else {
+            lista.forEach(p -> {
+                System.out.printf("[PKMN] Nombre: %s | Tipo: %s | Debilidades: %s%n",
+                        p.getNombre(), p.getTipo(), p.getDebilidad());
+            });
+        }
+    }
 }
